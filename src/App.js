8563b9ab10155f3a06d36ccda822bb97be088577
Hoang Hoa <kieuhoa256@gmail.com>
2022-03-31 14:46:45 +0700

    Checkout Page Update UI

--- src/App.js
+++ src/App.js
@@ -3,12 +3,13 @@ import "./App.css";
 import HomeAdmin from "./Admin/Component/HomeAdmin";
 import Home from "./User/pages/Home";
 import { Routes, Route, Outlet } from "react-router-dom";
-
+import Checkout from "./User/pages/checkout/Checkout";
 const App = () => {
   return (
     <Routes>
       <Route path="/" element={<Outlet />}>
         <Route index element={<Home />} />
+        <Route path="/checkout" element={<Checkout />} />
       </Route>
       <Route path="/admin" element={<Outlet />}>
         <Route index element={<HomeAdmin />} />
--- src/User/pages/checkout/Checkout.jsx
+++ src/User/pages/checkout/Checkout.jsx
@@ -104,8 +104,99 @@ const Checkout = () => {
                       </div>
                     </div>
                   </div>
+                  <div className="message_box color-info">
+                    <ol>
+                      <li>
+                        Quý khách vui lòng kiểm tra lại kỹ một lần nữa{" "}
+                        <strong>Thông tin giao hàng</strong> trước khi bấm nút{" "}
+                        <strong>Đặt hàng.</strong>
+                      </li>
+                      <li>
+                        Nhân viên Shop sẽ xác nhận đơn hàng trong vòng tối đa 4
+                        giờ làm việc (không kể Chủ nhật và Ngày lễ).
+                      </li>
+                    </ol>
+                    <p>
+                      <ul>
+                        <li>
+                          Điện thoại đặt hàng nhanh: 1800.6879 (miễn cước)
+                        </li>
+                        <li>Email: cskh@shoeshop.com.vn</li>
+                      </ul>
+                    </p>
+                  </div>
                 </div>
-                <div className="col-md-6"></div>
+                <div
+                  className="col-md-6"
+                  style={{ marginLeft: "20px", width: "500px" }}
+                >
+                  <h3>Chi tiết đơn hàng</h3>
+                  <table className="shop_table">
+                    <thead>
+                      <tr>
+                        <th>Sản phẩm</th>
+                        <th>Tạm tính</th>
+                      </tr>
+                    </thead>
+                    <tbody>
+                      <tr>
+                        <td>Giày nam</td>
+                        <td>500.000₫</td>
+                      </tr>
+                    </tbody>
+                    <tfoot>
+                      <tr>
+                        <td>
+                          <b>Tạm tính</b>
+                        </td>
+                        <td>500.000₫</td>
+                      </tr>
+                      <tr>
+                        <td>
+                          <b>Giao hàng</b>
+                        </td>
+                        <td>Miễn phí</td>
+                      </tr>
+                      <tr>
+                        <td>
+                          <b>Tổng</b>
+                        </td>
+                        <td>500.000₫</td>
+                      </tr>
+                    </tfoot>
+                  </table>
+
+                  <div className="checkout-payment">
+                    <ul className="payment_method">
+                      <li>
+                        Thanh toán sau khi nhận hàng (COD){" "}
+                        <img
+                          src="https://www.viettelidc.com.vn/Themes/itmetech/img/cash@2x.png"
+                          alt="cash"
+                          height="32"
+                        />
+                      </li>
+                      <li>
+                        Thanh toán bằng ví điện tử{" "}
+                        <img
+                          src="https://www.viettelidc.com.vn/Themes/itmetech/img/group-3@2x.png"
+                          height="32"
+                          alt="vnpay"
+                        />
+                      </li>
+                    </ul>
+                    <div className="payment_box">
+                      <p>
+                        Tất cả đơn hàng từ Shoe Shop đều hỗ trợ thử sản phẩm
+                        trước khi thanh toán. <br />
+                        Nếu không vừa size hoặc không ưng ý, vui lòng gửi trả
+                        lại nhân viên giao nhận mà không phát sinh thêm bất kỳ
+                        chi phí nào.
+                      </p>
+                    </div>
+                    <button className="place-order button">Đặt hàng</button>
+                  </div>
+                </div>
               </form>
             </div>
           </div>
