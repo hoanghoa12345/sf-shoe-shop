d85f917bc727793992607967c100db17c38f7acb
Hoang Hoa <kieuhoa256@gmail.com>
2022-04-15 11:05:28 +0700

    Fix header navbar

--- src/User/components/Header/index.jsx
+++ src/User/components/Header/index.jsx
@@ -1,46 +1,81 @@
-import { BsFillSuitHeartFill } from 'react-icons/bs';
-import { FaShoppingCart, FaUserAlt } from 'react-icons/fa';
-import { Link, NavLink } from 'react-router-dom';
-import './style.scss';
+import { BsFillSuitHeartFill } from "react-icons/bs";
+import { FaShoppingCart, FaUserAlt } from "react-icons/fa";
+import { Link, NavLink } from "react-router-dom";
+import "./style.scss";
 
 function Header() {
-    return (
-        <header className="header">
-            <div className="header__main container">
-                <div className="logo">
-                    <Link to='/'>ShoesShop</Link>
-                </div>
-                <ul className="header__menu">
-                    <li className="header__item">
-                        <NavLink to='/' className={({isActive}) => isActive ? 'header__item-active' : ''}>Home</NavLink>
-                    </li>
-                    <li className="header__item">
-                        <NavLink to='/product' className={({isActive}) => isActive ? 'header__item-active' : ''}>Product</NavLink>
-                    </li>
-                    <li className="header__item">
-                        <NavLink to='/fearured' className={({isActive}) => isActive ? 'header__item-active' : ''}>Fearured</NavLink>
-                    </li>
-                    <li className="header__item">
-                        <NavLink to='/blog' className={({isActive}) => isActive ? 'header__item-active' : ''}>Blog</NavLink>
-                    </li>
-                    <li className="header__item">
-                        <NavLink to='/news' className={({isActive}) => isActive ? 'header__item-active' : ''}>News</NavLink>
-                    </li>
-                </ul>
-                <div className="header__actions">
-                    <Link to='/heart' className="header__heart">
-                        <BsFillSuitHeartFill />
-                    </Link>
-                    <Link to='/cart' className="header__cart">
-                        <FaShoppingCart />
-                    </Link>
-                    <Link to='/signin' className="header__user">
-                        <FaUserAlt />
-                    </Link>
-                </div>
-            </div>
-        </header>
-     );
+  return (
+    <header className="header">
+      <div className="header__main container">
+        <div className="logo">
+          <Link to="/">ShoesShop</Link>
+        </div>
+        <ul className="header__menu">
+          <li className="header__item">
+            <NavLink
+              to="/"
+              className={({ isActive }) =>
+                isActive ? "header__item-active" : ""
+              }
+            >
+              Home
+            </NavLink>
+          </li>
+          <li className="header__item">
+            <NavLink
+              to="/product"
+              className={({ isActive }) =>
+                isActive ? "header__item-active" : ""
+              }
+            >
+              Product
+            </NavLink>
+          </li>
+          <li className="header__item">
+            <NavLink
+              to="/featured"
+              className={({ isActive }) =>
+                isActive ? "header__item-active" : ""
+              }
+            >
+              Featured
+            </NavLink>
+          </li>
+          <li className="header__item">
+            <NavLink
+              to="/blog"
+              className={({ isActive }) =>
+                isActive ? "header__item-active" : ""
+              }
+            >
+              Blog
+            </NavLink>
+          </li>
+          <li className="header__item">
+            <NavLink
+              to="/news"
+              className={({ isActive }) =>
+                isActive ? "header__item-active" : ""
+              }
+            >
+              News
+            </NavLink>
+          </li>
+        </ul>
+        <div className="header__actions">
+          <Link to="/heart" className="header__heart">
+            <BsFillSuitHeartFill />
+          </Link>
+          <Link to="/cart" className="header__cart">
+            <FaShoppingCart />
+          </Link>
+          <Link to="/signin" className="header__user">
+            <FaUserAlt />
+          </Link>
+        </div>
+      </div>
+    </header>
+  );
 }
 
 export default Header;
