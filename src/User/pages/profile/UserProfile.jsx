d7d7b560ca648d126afd2054c1f2694cb0667053
Hoang Hoa <kieuhoa256@gmail.com>
2022-04-22 10:37:25 +0700

    Add update user profile

--- src/User/pages/profile/UserProfile.jsx
+++ src/User/pages/profile/UserProfile.jsx
@@ -1,6 +1,8 @@
-import React from "react";
+import React, { useState } from "react";
 import "./UserProfile.scss";
+import { MdOutlineClose } from "react-icons/md";
 const UserProfile = () => {
+  const [open, setOpen] = useState(false);
   const profile = {
     name: "Test User",
     username: "testuser",
@@ -9,6 +11,9 @@ const UserProfile = () => {
     imageUrl:
       "https://th.bing.com/th/id/OIP.udIfmXkDTzwuDF4YKPHBPgHaHk?pid=ImgDet&rs=1",
     phone_number: "09459***789",
+  };
+  const submitHandler = (e) => {
+    e.preventDefault();
   };
   return (
     <div className="profile container">
@@ -38,13 +43,82 @@ const UserProfile = () => {
               </li>
             </ul>
             <div className="profile__box__button">
-              <button className="btn btn-primary w-60">
+              <button
+                onClick={() => setOpen(true)}
+                type="buttons"
+                className="btn btn-primary w-60"
+              >
                 Cập nhật thông tin
               </button>
             </div>
           </div>
         </div>
       </main>
+      {open && (
+        <>
+          <div className="details-modal-overlay"></div>
+          <div className="details-modal">
+            <div className="details-modal-title">
+              <h1>Cập nhật thông tin cá nhân</h1>
+              <button
+                type="button"
+                className="details-modal-close"
+                onClick={() => setOpen(false)}
+              >
+                <MdOutlineClose size="20" />
+              </button>
+            </div>
+
+            <div className="details-modal-content">
+              <form onClick={submitHandler}>
+                <div className="form-group">
+                  <label htmlFor="email">Email</label>
+                  <input
+                    type="email"
+                    name="email"
+                    id="email"
+                    placeholder="email"
+                  />
+                </div>
+                <div className="form-group">
+                  <label htmlFor="name">Name</label>
+                  <input type="name" name="name" id="name" placeholder="name" />
+                </div>
+                <div className="form-group">
+                  <label htmlFor="password">password</label>
+                  <input
+                    type="password"
+                    name="password"
+                    id="password"
+                    placeholder="Password"
+                  />
+                </div>
+                <div className="form-group">
+                  <label htmlFor="confirmed_password">Confirmed password</label>
+                  <input
+                    type="password"
+                    name="confirmed_password"
+                    id="confirmed_password"
+                    placeholder="Confirmed password"
+                  />
+                </div>
+                <div className="button-group">
+                  <button
+                    onClick={() => setOpen(false)}
+                    className="btn-outline"
+                    type="button"
+                  >
+                    Đóng
+                  </button>
+                  <button className="btn-primary" type="submit">
+                    Lưu thay đổi
+                  </button>
+                </div>
+              </form>
+            </div>
+          </div>
+        </>
+      )}
     </div>
   );
 };
