d63c2c27dc7a46bc51d99a996754592ce737b9b3
Hoang Hoa <kieuhoa256@gmail.com>
2022-04-22 10:37:25 +0700

    Add update user profile

--- src/User/pages/profile/UserProfile.jsx
+++ src/User/pages/profile/UserProfile.jsx
@@ -1,6 +1,15 @@
-import React from "react";
+import React, { useState } from "react";
 import "./UserProfile.scss";
+import { MdOutlineClose } from "react-icons/md";
+import { ToastContainer, toast } from "react-toastify";
 const UserProfile = () => {
+  const [open, setOpen] = useState(false);
+  const [editUser, setEditUser] = useState({
+    email: "",
+    name: "",
+    password: "",
+    confirmed_password: "",
+  });
   const profile = {
     name: "Test User",
     username: "testuser",
@@ -10,8 +19,23 @@ const UserProfile = () => {
       "https://th.bing.com/th/id/OIP.udIfmXkDTzwuDF4YKPHBPgHaHk?pid=ImgDet&rs=1",
     phone_number: "09459***789",
   };
+  const openModalEdit = () => {
+    setEditUser({
+      email: profile.email,
+      name: profile.name,
+      password: "",
+      confirmed_password: "",
+    });
+    setOpen(true);
+  };
+  const submitHandler = (e) => {
+    e.preventDefault();
+    toast.success("Cập nhật thông tin thành công");
+    setOpen(false);
+  };
   return (
     <div className="profile container">
+      <ToastContainer />
       <aside className="profile__sidebar">
         <div className="profile__avatar">
           <img src={profile.imageUrl} className="avatar__image" alt="" />
@@ -38,13 +62,106 @@ const UserProfile = () => {
               </li>
             </ul>
             <div className="profile__box__button">
-              <button className="btn btn-primary w-60">
+              <button
+                onClick={openModalEdit}
+                type="buttons"
+                className="btn btn-primary w-60"
+              >
                 Cập nhật thông tin
               </button>
             </div>
           </div>
         </div>
       </main>
+      {open && (
+        <>
+          <div className="details-modal-overlay"></div>
+          <div className="details-modal">
+            <div className="details-modal-title">
+              <h1>Cập nhật thông tin cá nhân</h1>
+              <button
+                type="button"
+                className="details-modal-close"
+                onClick={() => setOpen(false)}
+              >
+                <MdOutlineClose size="20" />
+              </button>
+            </div>
+
+            <div className="details-modal-content">
+              <form onSubmit={submitHandler}>
+                <div className="form-group">
+                  <label htmlFor="email">Email</label>
+                  <input
+                    type="email"
+                    name="email"
+                    id="email"
+                    placeholder="Email"
+                    onChange={(e) =>
+                      setEditUser({ ...editUser, email: e.target.value })
+                    }
+                    value={editUser.email}
+                  />
+                </div>
+                <div className="form-group">
+                  <label htmlFor="name">Name</label>
+                  <input
+                    type="name"
+                    name="name"
+                    id="name"
+                    placeholder="Name"
+                    onChange={(e) =>
+                      setEditUser({ ...editUser, name: e.target.value })
+                    }
+                    value={editUser.name}
+                  />
+                </div>
+                <div className="form-group">
+                  <label htmlFor="password">Password</label>
+                  <input
+                    type="password"
+                    name="password"
+                    id="password"
+                    placeholder="Password"
+                    onChange={(e) =>
+                      setEditUser({ ...editUser, password: e.target.value })
+                    }
+                    value={editUser.password}
+                  />
+                </div>
+                <div className="form-group">
+                  <label htmlFor="confirmed_password">Confirmed password</label>
+                  <input
+                    type="password"
+                    name="confirmed_password"
+                    id="confirmed_password"
+                    placeholder="Confirmed password"
+                    onChange={(e) =>
+                      setEditUser({
+                        ...editUser,
+                        confirmed_password: e.target.value,
+                      })
+                    }
+                    value={editUser.confirmed_password}
+                  />
+                </div>
+                <div className="button-group">
+                  <button
+                    onClick={() => setOpen(false)}
+                    className="btn-outline"
+                    type="button"
+                  >
+                    Đóng
+                  </button>
+                  <button className="btn-primary" type="submit">
+                    Lưu thay đổi
+                  </button>
+                </div>
+              </form>
+            </div>
+          </div>
+        </>
+      )}
     </div>
   );
 };
