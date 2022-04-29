3356b2d2175406ba65dcac54a97fe0c7b6999029
sangdangyeu <99857335+sangdangyeu@users.noreply.github.com>
2022-04-29 13:57:22 +0700

    29/4

--- src/User/components/ListProduct/ListProduct.js
+++ src/User/components/ListProduct/ListProduct.js
@@ -2,44 +2,46 @@ import axios from 'axios'
 import React, { useEffect, useState } from 'react'
 import { Link, useNavigate } from 'react-router-dom'
 import { Pagination } from 'swiper'
-
 import './ListProduct.css'
 import Loading from './../Loading/index'
 
+
 function ListProduct() {
-    const navigate = useNavigate()
-    const [products, setProducts] = useState([])
+    const navigate = useNavigate();
+    const [products, setProducts] = useState([]);
+    const [productsTemp, setProductsTemp] = useState([]);
+
     const categories = [
         {
-            id: 1,
-            name: 'Giày Tây'
-        },
-        {
-            id: 2,
-            name: 'Giày Bốt Nam'
-        },
-        {
-            id: 3,
-            name: 'Giày lười nam'
-        },
-        {
-            id: 4,
-            name: 'Giày Vans'
-        }
-    ]
+            id: "Sneaker",
+            name: "Giày Tây",
+          },
+          {
+            id: "Shoe",
+            name: "Giày Bốt Nam",
+          },
+          {
+            id: "GUCI",
+            name: "Giày lười nam",
+          },
+          {
+            id: "Shoe",
+            name: "Giày Vans",
+          }
+        ]
 
     const getProductAPI = async () => {
         return await axios.get(
             'https://sf-shoe-shop-be.herokuapp.com/api/products/'
-        )
+        );
     }
     useEffect(() => {
         const getProduct = async () => {
-            const { data } = await getProductAPI()
-            setProducts([...data])
-            console.log(data)
+            const { data } = await getProductAPI();
+            setProducts([...data]);
+            setProductsTemp([...data]);
         }
-        getProduct()
+        getProduct();
     }, [])
 
     // const[pagination,setPagination] = useState({
@@ -51,9 +53,9 @@ function ListProduct() {
     //   console.log("new page:", newPage);
     // }
     const hanldeClick = catItem => {
-        const result = products.filter(curData => {
-            return curData.categori === catItem
-        })
+        const result = productsTemp.filter(curData => {
+            return curData.category === catItem;
+        });
         setProducts(result)
     }
     return (
@@ -61,24 +63,14 @@ function ListProduct() {
             <div className="category__left">
                 <h4 className="category__heading">KHÁM PHÁ DANH MỤC</h4>
                 <ul className="category__menu">
-                    <li className="category__item">
-                        <span
-                            to={`/category`}
-                            onClick={() => setProducts('catId')}
-                        >
-                            ALL
-                        </span>
+                    <li className="category__item"  >
+                        <span to={`/category`} onClick={() => setProducts(productsTemp)}>ALL</span>
                     </li>
-                    {categories.map(item => {
+                    {categories.map((item,index) => {
                         const { id } = item
                         return (
-                            <li className="category__item" key={id}>
-                                <span
-                                    to={`/category/${item.id}`}
-                                    onClick={() => hanldeClick(item.id)}
-                                >
-                                    {item.name}
-                                </span>
+                            <li className="category__item" key={index} >
+                                <span to={`/category/${id}`} onClick={() => hanldeClick(item.id)}>{item.name}</span>
                             </li>
                         )
                     })}
@@ -88,14 +80,16 @@ function ListProduct() {
                 <div className="category__list">
                     {products.length !== 0 ? (
                         products.map(item => (
-                            <div className="category__product" key={item.id}>
-                                <div>
+                            <div className="category__product" key={item._id}>
+                               <div>
                                     <img
-                                        className="category__img "
+                                        className="category__image "
+                                       
                                         src={item.image}
                                         alt=""
                                     />
                                 </div>
+                               
                                 <div className="category__name">
                                     {item.name}
                                 </div>
